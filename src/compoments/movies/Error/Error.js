5c12893bb2158de1f9f755d885e3a067245cefb7
Gulnaz Nasrtdinova <gulnaz0206@yandex.ru>
2023-08-10 16:41:54 +0500

    update Error.js

--- src/compoments/movies/Error/Error.js
+++ src/compoments/movies/Error/Error.js
@@ -7,11 +7,13 @@ function Error ({setViewHeader}) {
         setViewHeader(false)
     }, [])
     return (
+        <main>
         <section className="error">
             <h1 className="error__title">404</h1>
             <p className="error__text">Страница не найдена</p>
             <a className="error__back" href="/">Назад</a>
         </section>
+        </main>
     );
 }
 export default Error;
