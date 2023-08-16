3c0f6920401555653dd788e43428b2f4edb4d371
Gulnaz Nasrtdinova <gulnaz0206@yandex.ru>
2023-08-16 22:27:48 +0500

    исправления

--- src/compoments/Portfolio/Portfolio.js
+++ src/compoments/Portfolio/Portfolio.js
@@ -6,20 +6,18 @@ function Portfolio() {
         <section className='portfolio'>
             <h2 className='portfolio__title'>Портфолио</h2>
             <ul className='portfolio__container'>
-                <li className='portfolio__list'>
-                    <a className='portfolio__item link' href='https://github.com/gulnaz0206/how-to-learn' target="_blank" rel='noreferrer'>Статичный сайт
-                    <button className='portfolio__button' type="button"></button>
-                    </a>
+                <li className='portfolio__list'> <a className='portfolio__item link' href='https://github.com/gulnaz0206/how-to-learn' target="_blank" rel='noreferrer'>Статичный сайт</a>
+                <button className='portfolio__button' type="button"></button>
                 </li>
                 <li className='portfolio__list'>
                     <a className='portfolio__item link' href='https://github.com/gulnaz0206/russian-travel' target="_blank" rel='noreferrer'>Адаптивный сайт
-                    <button className='portfolio__button' type="button"></button>
                     </a>
+                    <button className='portfolio__button' type="button"></button>
                 </li>
                 <li className='portfolio__list'>
                     <a className='portfolio__item link' href='https://github.com/gulnaz0206/react-mesto-api-full-gha' target="_blank" rel='noreferrer'>Одностраничное приложение
-                    <button className='portfolio__button' type="button"></button>
                     </a>
+                    <button className='portfolio__button' type="button"></button>
                 </li>
             </ul>
         </section>
--- src/compoments/Promo/Promo.js
+++ src/compoments/Promo/Promo.js
@@ -7,21 +7,23 @@ function Promo() {
                 <h1 className="promo__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
             <nav className="promo__navigation">
                 <ul className="promo__more">
+                <li className="promo__more-list">
+                    <a className="promo__navigation-button" href="#aboutProject">О проекте
+                    </a>
+                </li>
+                <li className="promo__more-list">
+                    <a className="promo__navigation-button" href="#tech">Технологии
+                    </a>
+                </li>
                     <li className="promo__more-list">
+                    <a className="promo__navigation-button" href="#aboutme">Студент
+                    </a>
+                </li>
+                    {/* <li className="promo__more-list">
                         <button className="promo__navigation-button" type="button">
-                            <a className="promo__navigation-button-link" href="#aboutProject">О проекте</a>
+                            <a className="promo__navigation-button-link" href="#aboutme"></a>
                         </button>
-                    </li>
-                    <li className="promo__more-list">
-                        <button className="promo__navigation-button" type="button">
-                            <a className="promo__navigation-button-link" href="#tech">Технологии</a>
-                        </button>
-                    </li>
-                    <li className="promo__more-list">
-                        <button className="promo__navigation-button" type="button">
-                            <a className="promo__navigation-button-link" href="#aboutme">Студент</a>
-                        </button>
-                        </li>
+                        </li> */}
                 </ul>
             </nav>
         </section>
